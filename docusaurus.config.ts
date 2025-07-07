d4258cc888c96a0481d61c80094e8abd31580c99
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 16:49:56 +0900

    update site URL and base path in docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://vtj-devops.github.io',
+  url: 'https://docs.kintai.virtualtech.jp/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/cloud-kintai-manual/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
