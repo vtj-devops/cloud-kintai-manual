975c11b33bc9a23c414b6caef1c52f10a0cc8d66
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 16:13:23 +0900

    remove community links from configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -104,23 +104,6 @@ const config: Config = {
           ],
         },
         {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'X',
-              href: 'https://x.com/docusaurus',
-            },
-          ],
-        },
-        {
           title: 'More',
           items: [
             {
