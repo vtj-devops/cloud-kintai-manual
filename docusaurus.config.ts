bb62f5563fb6b5b6ff58875aad31681a0eeb2ab9
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 16:21:14 +0900

    update site title to cloud kintai

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -71,7 +71,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'クラウド勤怠',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
