250e549005ded86429308f5624e7d37f770a5934
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 17:03:31 +0900

    update site URL in docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.kintai.virtualtech.jp/',
+  url: 'https://docs.kintai.virtualtech.jp',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
