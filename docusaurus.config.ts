5087609126410c4e652917f0df5106eb7905dd30
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 16:46:37 +0900

    update label from Tutorial to Docs in configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -81,7 +81,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Docs',
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
@@ -98,7 +98,7 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-              label: 'Tutorial',
+              label: 'Docs',
               to: '/docs/intro',
             },
           ],
