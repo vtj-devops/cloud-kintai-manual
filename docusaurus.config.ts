0abc910c9b3142320bbf9372fcddef195b8fbfd7
Ishimoto Tatsuya <ishimoto@virtualtech.jp>
2025-07-07 16:17:49 +0900

    update GitHub link in configuration to point to cloud kintai repository

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -85,7 +85,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/vtj-devops/cloud-kintai',
           label: 'GitHub',
           position: 'right',
         },
